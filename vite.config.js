52a7eb10955711d10d6cc3618e1a83325df38d89
Abdul Musavir Chukkan <musavirc17@gmail.com>
2025-05-30 04:10:33 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'query-vendor': ['@tanstack/react-query'],
+          'style-vendor': ['styled-components'],
+          'icon-vendor': ['lucide-react'],
+        },
+      },
+    },
+  },
 })
